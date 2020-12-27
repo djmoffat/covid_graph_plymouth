ab0ddcc74927bf8092bdc438c4e2e4a9a2498b35
David Moffat <dmoffat@pm7-0752.lan>
2020-12-27 16:17:47 +0000

    plot from data frame, and include devon cases

--- main.py
+++ main.py
@@ -1,52 +1,64 @@
-#! /usr/local/opt/python/libexec/bin/python
+#!/usr/local/opt/python/libexec/bin/python
 
 import json
 import matplotlib.pyplot as plt
 import requests
 import numpy as np
+import pandas as pd
+from scipy.signal import savgol_filter
+
 import datetime
 
+url = {}
+population = {}
 
-plym_url = 'https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=utla;areaName=Plymouth&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newCasesBySpecimenDate%22:%22newCasesBySpecimenDate%22,%22cumCasesBySpecimenDate%22:%22cumCasesBySpecimenDate%22%7D&format=json'
 lockdowns = [(datetime.date(2020,11,5),datetime.date(2020,12,2)), (datetime.date(2020,3,23),datetime.date(2020,7,4)),]
 verbose = False
 
-def fetch_data():
-	post_return = requests.get(plym_url)
-	post_data = json.loads(post_return.text)
-	# print(post_data)
-	return post_data
 
-def parse_data(data):
-	date_data = [datetime.datetime.fromisoformat(d['date']) for d in data]
-	cases_data = [d['newCasesBySpecimenDate'] for d in data]
+def init_urls():
+	url['Plymouth'] = 'https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=utla;areaName=Plymouth&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newCasesBySpecimenDate%22:%22newCasesBySpecimenDate%22,%22cumCasesBySpecimenDate%22:%22cumCasesBySpecimenDate%22%7D&format=json'
+	url['Devon'] = 'https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=utla;areaName=Devon&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newCasesBySpecimenDate%22:%22newCasesBySpecimenDate%22,%22cumCasesBySpecimenDate%22:%22cumCasesBySpecimenDate%22%7D&format=json'
+	# url['England'] = 'https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation;areaName=England&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newCasesBySpecimenDate%22:%22newCasesBySpecimenDate%22,%22cumCasesBySpecimenDate%22:%22cumCasesBySpecimenDate%22%7D&format=json'
+
+def fetch_data(region):
+	post_return = requests.get(url[region])
+	post_data = json.loads(post_return.text)
 	if verbose:
-		for date,case in sorted(zip(date_data, cases_data), key = lambda x: x[0]):
-			print(date,case)
-	cases_data = average_smoothing(cases_data, window_size=7)
-	return(date_data, cases_data)
+		print(post_data)
+	return post_data['data']
+
+def fetch_parse_data(region):
+	post_data = fetch_data(region)
+	df_data = {}
+	df_data['date'] = [datetime.datetime.fromisoformat(d['date']) for d in post_data]
+	cases = [d['newCasesBySpecimenDate'] for d in post_data]
+	df_data[region+'_cases'] = average_smoothing(cases, window_size=7)
+	df = pd.DataFrame(data=df_data)
+	if verbose:
+		print(df)
+	return(df)
 
 def average_smoothing(y, window_size=3):
-	from scipy.signal import savgol_filter
 	return savgol_filter(y, window_size, 3)
 
-def plot_data(data, highlights, output_figure='images/plymouth_covid_graph.png'):
-	plt.plot(data[0], data[1])
-	plt.xticks(rotation=45)
+def plot_data(df, highlights=lockdowns, output_figure='images/plymouth_covid_graph.png'):
+	df.plot(x='date')
 	for h in highlights:
 		plt.axvspan(h[0], h[1], color='grey', alpha=0.5)
-	plt.ylim(bottom=0)
-	plt.xlabel('Date') 
 	plt.ylabel('Number of Cases') 
-	plt.title('Plymouth Covid Cases')
-	plt.legend(["Number of New Cases Per Day", "National Locks Downs"]);
-	plt.gcf().autofmt_xdate()
+	plt.title('Covid Cases')
 	plt.savefig(output_figure)
 
 def main():
-	data = fetch_data()
-	data = parse_data(data['data'])
-	plot_data(data, lockdowns)
+	df = pd.DataFrame(data={'date':[]})
+	data = {}
+	init_urls()
+
+	for region in url:
+		data[region] = fetch_parse_data(region)
+		df = (pd.merge(df, data[region], on='date', how='outer'))
+	plot_data(df)
 
 if __name__ == "__main__":
 	main()
